bbed8cdba129c4d5b5dc24f89ebb729a6db43bca
Bluecrow UBUNTU DESKTOP <bluecrow.dev@gmail.com>
2021-12-04 21:07:23 +0000

    NextJS base v1.0

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import type { NextPage } from 'next';
+
+const Page: NextPage = () => {
+  return (
+    <>
+      <h1>Hello world</h1>
+    </>
+  );
+};
+
+export default Page;
